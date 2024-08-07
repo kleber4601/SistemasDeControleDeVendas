8fa84672de653128371feddc0d405579631861e9
kleber Santos <ksd.campos@gmail.com>
2024-08-06 23:14:52 -0300

    testando

--- src/interfacesDo/PainelPrincipal.java
+++ src/interfacesDo/PainelPrincipal.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class PainelPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -32,11 +40,72 @@ public class PainelPrincipal extends JFrame {
 	 */
 	public PainelPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 683, 470);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nome:");
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNewLabel.setBounds(10, 32, 77, 21);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setBounds(56, 24, 234, 28);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setBounds(366, 32, 291, 321);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Preço:");
+		lblNewLabel_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblNewLabel_1.setBounds(10, 75, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setEditable(false);
+		textField_2.setBounds(56, 64, 234, 28);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Descrição:");
+		lblNewLabel_2.setBounds(10, 134, 55, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setEditable(false);
+		textField_3.setBounds(66, 103, 224, 246);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Sair");
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnNewButton.setBounds(568, 386, 89, 34);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Vender");
+		btnNewButton_1.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnNewButton_1.setBounds(66, 386, 89, 34);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Voltar");
+		btnNewButton_2.setFont(new Font("Arial", Font.PLAIN, 12));
+		btnNewButton_2.setBounds(469, 386, 89, 34);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("New button");
+		btnNewButton_3.setBounds(353, 392, 106, 28);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("New button");
+		btnNewButton_4.setBounds(222, 392, 89, 23);
+		contentPane.add(btnNewButton_4);
 	}
-
 }
